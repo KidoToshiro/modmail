e5f6dd66e32aef44949d475a3ab244bbc47f7a0a
Vũ Văn Tiến <tienvu.061298@gmail.com>
2018-12-30 13:16:05 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -136,7 +136,7 @@ class Modmail(commands.Bot):
         User ID: {self.user.id}
         ---------------
         '''))
-        await self.change_presence(game=discord.Game(name="Memes", type = 3))
+        await self.setPresence(game=discord.Game(name="Message me for help!", type = 3))
 
     def overwrites(self, ctx, modrole=None):
         '''Permision overwrites for the guild.'''
